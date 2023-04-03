54a25ca6e4f12dfe3e3d8eca694fbc6854c3a84b
MemmedovAslan <76853345+MemmedovAslan@users.noreply.github.com>
2023-04-03 04:09:52 +0400

    Add files via upload

---
+++ Task/Task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Task;
+
+namespace JPhone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StandardJPhone phone = new StandardJPhone("Aslan");
+
+            phone.AddAccount("Vusal");
+            Console.WriteLine();
+            phone.AddAccount("Emin");
+            Console.WriteLine();
+            phone.AddAccount("Yusif");
+            Console.WriteLine();
+
+            phone.DeleteAccount("Emin");
+            phone.DeleteAccount("Emin");
+            phone.DeleteAccount("Emin");
+            phone.DeleteAccount("Emin");
+
+            Console.WriteLine("\n************************************\n");
+
+            phone.PrintAllAccounts();
+            Console.WriteLine();
+
+            phone.Call("423456");
+            Console.WriteLine();
+
+            phone.PrintInfo();
+
+        }
+    }
+}
+
+    
---
+++ Task/Task/StandardJPhone.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class StandardJPhone
+    {
+        string name;
+        int storage = 32;
+        ArrayList accounts = new ArrayList();
+        public StandardJPhone(string name)
+        {
+            this.name = name;
+        }
+        public void Call(string number)
+        {
+            if ((number.Length >= 6 && number.Length <= 8) && (number[0] == '4' || number[0] == '6'))
+                Console.WriteLine($"  Calling to the number {number}");
+            else
+                Console.WriteLine("  Invalid phone number, please check the number!");
+        }
+        public void AddAccount(string name)
+        {
+            if (accounts.Count < 3)
+            {
+                accounts.Add(name);
+                Console.WriteLine($"  +  Account {name} was added");
+            }
+            else
+                Console.WriteLine("  You cannot add more accounts, limit is reached!");
+        }
+        public void PrintAllAccounts()
+        {
+            if (accounts.Count > 0)
+            {
+                Console.WriteLine("  Accounts are:");
+                for (int i = 0; i < accounts.Count; i++)
+                {
+                    Console.WriteLine("  >  " + accounts[i]);
+                }
+            }
+            else
+                Console.WriteLine("  No user accounts to display");
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"  JPhone is called ’{name} overpriced phone’, it has {storage}GB of memory and {accounts.Count} user accounts");
+        }
+
+        //Bonus
+        public void DeleteAccount(string name)
+        {
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                if (accounts[i] == name)
+                {
+                    Console.WriteLine($"  -  {name} was deleted!");
+                    break;
+                }
+                //else
+                //{
+                //    Console.WriteLine("  There is no account like that!");
+                //    break;
+                //}
+            }
+            accounts.Remove(name);
+        }
+    }
+}
